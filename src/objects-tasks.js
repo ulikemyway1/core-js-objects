8cdbe11e9a43b6151e2933588d8cfeb9347bfad7
ulikemyway1 <calium@outlook.com>
2024-02-21 19:58:33 +0300

    feat: implement shallowCopy and mergeObjects functions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,10 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const newObj = {};
+  const copy = Object.assign(newObj, obj);
+  return copy;
 }
 
 /**
@@ -32,8 +34,22 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const newObj = {};
+  const keys = [];
+  objects.forEach((object) => {
+    keys.push(Object.entries(object));
+  });
+  const flatKey = keys.flat();
+  flatKey.forEach((pair) => {
+    const [key, value] = pair;
+    if (newObj[key]) {
+      newObj[key] += value;
+    } else {
+      newObj[key] = value;
+    }
+  });
+  return newObj;
 }
 
 /**
